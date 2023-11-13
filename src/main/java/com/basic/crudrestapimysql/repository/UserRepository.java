8c2f2f3e9b77dcce26d0341eb8aca1fcaa940c7d
Veera-devp <saikam.raghava@gmail.com>
2023-11-13 21:43:58 +0530

    Exception handling for the API's

--- src/main/java/com/basic/crudrestapimysql/repository/UserRepository.java
+++ src/main/java/com/basic/crudrestapimysql/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.basic.crudrestapimysql.repository;
 import com.basic.crudrestapimysql.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long>
 {
-
+    Optional<User> findByEmail(String email);
 }
